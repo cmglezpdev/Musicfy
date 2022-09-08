b85a339f305d363d42c159328641fababd03cd0f
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-07 22:25:10 -0400

    read artist of firebase

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import { getFirestore, collection, getDoc, doc, query, getDocs } from 'firebase/firestore';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 import { BannerHome } from '../../components/BannerHome/BannerHome';
+
 import './Home.scss';
 
 export const Home = () => {
+
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+
+    const arrayArtists = [];
+    const db = getFirestore();
+    const q = query(collection(db, "artists"));
+    getDocs(q).then(response => {
+      response?.docs?.forEach(artist => {
+        const data = artist.data();
+        data.id = artist.id;
+        arrayArtists.push(data);
+      })  
+    })
+
+    setArtists(arrayArtists);
+  }, [])
+
   return (
     <>
       <BannerHome />
