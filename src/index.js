97f5390d19889060e7fe800af24f9ccec704e1ff
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-10 02:22:31 -0400

    ...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-
 import { store } from './store/store';
 import App from './App';
+
 import 'semantic-ui-css/semantic.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.scss'
 
 const root = createRoot( document.querySelector('#root') );
-
 root.render( 
     <Provider store={store}>
         <App /> 
