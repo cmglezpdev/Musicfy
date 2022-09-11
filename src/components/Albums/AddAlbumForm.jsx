56dc8cf493315ba1de6d165f70031eeabe84e987
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-10 20:55:11 -0400

    add logic for the create album and save in forestore and storage

--- src/components/Albums/AddAlbumForm.jsx
+++ src/components/Albums/AddAlbumForm.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { Form, Input, Button, Dropdown, Image } from 'semantic-ui-react';
 import { useDropzone } from 'react-dropzone';
-import { useFirebaseFirestore } from '../../Hooks';
+import { v4 as uuidv4 } from 'uuid';
+import { toast } from 'react-toastify';
+import { useFirebaseFirestore, useFirebaseStorage,useForm } from '../../Hooks';
 import { firebaseApp } from '../../utils';
+import { closeModal } from '../../actions/uiActions';
 
 import NoImage from '../../assets/png/no-image.png';
 import './addAlbumForm.scss';
@@ -12,7 +16,15 @@ export const AddAlbumForm = () => {
     const [albumImage, setAlbumImage] = useState(null);
     const [artistsOptions, setArtistsOptions] = useState([]);
     const [file, setFile] = useState(null);
-    const { getCollectionList } = useFirebaseFirestore(firebaseApp);
+    const [isLoading, setIsLoading] = useState(false);
+    const dispatch = useDispatch();
+    const { getCollectionList, setDocument } = useFirebaseFirestore(firebaseApp);
+    const { uploadFile } = useFirebaseStorage(firebaseApp);
+
+    const [formData, handleInputChange, reset, handleUpdateField] = useForm({
+        name: "",
+        artist: "",
+    });
 
     useEffect(() => {
         getCollectionList("artists")
@@ -42,9 +54,40 @@ export const AddAlbumForm = () => {
         onDrop
     })
 
+    const onSubmit = async () => {
+        if( !formData.name || !formData.artist ) {
+            toast.warning("The album and artist are required!")
+            return;
+        }
+        if( !file ) {
+            toast.warning("The album image is required!");
+            return;
+        }
+        setIsLoading(true);
+        
+        try {
+            const fileName = uuidv4();
+            await uploadFile(`albums/${fileName}`, file, { contentType: file.type })
+            await setDocument("albums", {
+                name: formData.name,
+                artist: formData.artist,
+                banner: fileName
+            })
+            toast.success("Successfully album created!");
+            resetForm();
+        } catch (error) {
+            toast.error("Error to create the album!");            
+        }
 
-    const onSubmit = () => {
-        console.log("Sending Form");
+        setIsLoading(false);
+        dispatch(closeModal());
+    }
+
+    const resetForm = () => {
+        reset();
+        setFile(null);
+        setAlbumImage(null);
+        setArtistsOptions([]);
     }
 
     return (
@@ -61,7 +104,11 @@ export const AddAlbumForm = () => {
                 </Form.Field>
 
                 <Form.Field className="album-inputs" width={11}>
-                    <Input placeholder="Album name" />
+                    <Input 
+                        placeholder="Album name" 
+                        name="name"
+                        onChange={handleInputChange}    
+                    />
                     
                     <Dropdown 
                         placeholder='The album pertense...' 
@@ -70,10 +117,11 @@ export const AddAlbumForm = () => {
                         selection
                         lazyLoad
                         options={artistsOptions}
+                        onChange={ (e, data) => handleUpdateField("artist", data.value)}
                     />
                 </Form.Field>
             </Form.Group>
-            <Button type='submit'>
+            <Button type='submit' loading={isLoading}>
                 Create Album
             </Button>
         </Form>
