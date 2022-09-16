6004580888ee1e5d450b9f7d70d5f448cba6f803
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-15 20:45:37 -0400

    feat(Root): add the info of the albums in its page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,4 @@ export { Auth } from './Auth/Auth';
 export { Home } from './Home/Home';
 export { Settings } from './Settings/Settings';
 export { Albums } from './Albums/Albums'
+export { Album } from './Album/Album'
--- src/routes/ContentRoutes.jsx
+++ src/routes/ContentRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home, Artists, Artist, Settings, Albums } from '../pages'
+import { Home, Artists, Artist, Settings, Albums, Album } from '../pages'
 
 
 export const ContentRoutes = () => {
@@ -9,8 +9,9 @@ export const ContentRoutes = () => {
         <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path="/artists" element={<Artists /> } />
-            <Route path="/albums" element={<Albums /> } />
             <Route path="/artist/:id" element={<Artist /> } />
+            <Route path="/albums" element={<Albums /> } />
+            <Route path="/album/:id" element={<Album /> } />
             <Route path ="/settings" exact element={<Settings />} />
         </Routes>
     )
