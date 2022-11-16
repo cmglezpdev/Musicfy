6f6a9db859d6d3e0a8de54cbf9e7d71672d1a36a
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-16 09:39:00 -0500

    feat: add modal to upload songs

--- src/components/Songs/AddSongForm.jsx
+++ src/components/Songs/AddSongForm.jsx
@@ -1,32 +1,81 @@
-import { useState, useEffect } from 'react';
-import { Button, Dropdown, Form, Input } from 'semantic-ui-react';
+import { useState, useEffect, useCallback } from 'react';
+import { Button, Dropdown, Form, Icon, Input } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
+import { v4 as uuid } from 'uuid';
 import { firebaseApp } from '../../utils';
-import { useFirebaseFirestore } from '../../Hooks';
+import { useFirebaseFirestore, useFirebaseStorage, useForm } from '../../Hooks';
+
+import './addSongForm.scss';
+import { toast } from 'react-toastify';
 
 export const AddSongForm = () => {
 
-    const { getCollectionList } = useFirebaseFirestore(firebaseApp);
+    const { getCollectionList, setDocument } = useFirebaseFirestore(firebaseApp);
+    const { uploadFile } = useFirebaseStorage(firebaseApp); 
+    const [formData, handleInputChange, reset] = useForm({song: "", album: ""});
     const [albums, setAlbums] = useState([]);
-
+    const [file, setFile] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    
     useEffect(() => {
         getCollectionList("albums")
         .then(albs => {
-            const arralbums = albs.map(({ id, name }) => ({
-                key: id,
-                value: id,
-                text: name
-            }))
-
+            const arralbums = [];
+            albs.forEach(({ id, name }) => {
+                 arralbums.push({
+                    key: id,
+                    value: id,
+                    text: name
+                })
+            })
+            
             setAlbums(arralbums);
         })
-
+        
     }, [getCollectionList])
+    
+    const onDrop = useCallback(acceptFile => {
+        const file = acceptFile[0];
+        setFile(file);
+    }, [])
 
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: '.mp3',
+        noKeyboard: true,
+        onDrop
+    })
 
-
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
-        console.log("Sending Form");
+        if( formData.song === "" || formData.album === "" ) {
+            toast.warning('The song name and the album are required!');
+            return;
+        }
+        
+        // Send to firebase
+        try {
+            setIsLoading(true);
+            const result = await uploadFile(`songs/${uuid()}`, file, {contentType: file.type})
+            const data = {
+                    name: formData.song,
+                    album: formData.album,
+                    song: result.metadata.name
+                }
+            await setDocument("songs", data)
+            setIsLoading(false);
+            toast.success('Song uploading successfully!');
+            resetForm();
+        } catch (error) {
+            console.log(error);
+            toast.error("Error to upload the song. Try later!");
+            setIsLoading(false);
+        }
+    }
+
+    const resetForm = () => {
+        setFile(null);
+        reset();
     }
 
     return (
@@ -34,6 +83,8 @@ export const AddSongForm = () => {
             <Form.Field>
                 <Input 
                     placeholder="Song Name"
+                    name="song"
+                    onChange={handleInputChange}
                 />
             </Form.Field>
 
@@ -44,14 +95,24 @@ export const AddSongForm = () => {
                     selection
                     lazyLoad
                     options={albums}
+                    onChange={(e, data) => handleInputChange({target:{name:"album", value: data.value}})}
                 />
             </Form.Field>
             
             <Form.Field>
-                <h2>Upload Song</h2>
+                <div className="song-upload" { ...getRootProps() } >
+                    <input {...getInputProps} />
+                    <Icon name="cloud upload" className={file && 'load'} />
+                    <div>
+                        <p>Drag your song or click <span>here</span>.</p>
+                        {file && (
+                            <p>Uploaded Song: <span>{file.name}.mp3</span></p>
+                        )}
+                    </div>
+                </div>
             </Form.Field>
 
-            <Button type='submit'>
+            <Button type='submit' loading={isLoading}>
                 Upload Song
             </Button>
         </Form>
